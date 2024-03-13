f9e9610613e508e2756c910fe8225572c67d0bbb
Zachary Blackwood <zachary.blackwood@snowflake.com>
2024-03-13 17:12:38 -0400

    Real app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,83 @@
+from __future__ import annotations
+
 import streamlit as st
+from snowflake.snowpark.functions import count_distinct
 
-st.title(":rocket: Auto-deployment with Github Actions and SnowCLI")
+from common.get_data import get_events
+from common.utils import (
+    altair_time_series,
+    format_sql_from_df,
+    get_pandas_df,
+    tile,
+)
 
-st.write("Hello, world!")
+st.session_state["customers"] = st.session_state.get("customers", [])
+
+st.set_page_config(page_title="App Performance", page_icon="📈", layout="wide")
+st.write("# 📈 App Performance ")
+
+events = get_events("SAMPLE.PUBLIC.EVENT_DATA")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("## Overall Usage")
+
+    events_per_day = events.group_by("day").agg(
+        count_distinct("event_id").alias("events"),
+    )
+
+    events_per_week = events.group_by("week").agg(
+        count_distinct("event_id").alias("events"),
+    )
+
+    joined = events_per_day.join(
+        events_per_week,
+        events_per_day.day == events_per_week.week,
+        how="left",
+        lsuffix="_day",
+        rsuffix="_week",
+    ).drop("week")
+
+    df = get_pandas_df(joined, lowercase_columns=True)
+    df = df.melt("day").dropna()
+
+    events_plot = altair_time_series(df, "day", "value", "Date", "Number of events")
+
+    tile(df, "Events per day", events_plot, sql=format_sql_from_df(events_per_day))
+
+with col2:
+    st.write("## Active Customers")
+
+    customers_per_day = events.group_by("day").agg(
+        count_distinct("customer").alias("customers"),
+    )
+
+    customers_per_week = events.group_by("week").agg(
+        count_distinct("customer").alias("customers"),
+    )
+
+    joined = customers_per_day.join(
+        customers_per_week,
+        customers_per_day.day == customers_per_week.week,
+        how="left",
+        lsuffix="_day",
+        rsuffix="_week",
+    ).drop("week")
+
+    df = get_pandas_df(joined, lowercase_columns=True)
+    df = df.melt("day").dropna()
+    customers_plot = altair_time_series(
+        df,
+        "day",
+        "value",
+        "Date",
+        "Number of customers",
+    )
+
+    tile(
+        df,
+        "Customers per day",
+        customers_plot,
+        sql=format_sql_from_df(customers_per_day),
+    )
